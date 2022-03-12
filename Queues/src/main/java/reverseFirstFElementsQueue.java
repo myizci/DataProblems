da5dd3d3404ad31ebf25175cb7fd6ed76cc34f14
myizci <izci@hotmail.com>
2022-03-12 11:51:02 -0500

    Queue introduced

---
+++ Queues/src/main/java/reverseFirstFElementsQueue.java
@@ -0,0 +1,17 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class reverseFirstFElementsQueue {
+    public static void main(String[] args) {
+
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        queue.add(1); // add is used for Queue
+        queue.add(2);
+        System.out.println(queue.peek());
+    }
+
+    public void ReverseFirstK(){
+
+    }
+}
