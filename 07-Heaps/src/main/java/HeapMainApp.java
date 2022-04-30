74d9f759fb91da7dbf7503b88a34246e461ffff9
myizci <izci@hotmail.com>
2022-04-30 15:11:16 -0400

    Heap introduced

---
+++ 07-Heaps/src/main/java/HeapMainApp.java
@@ -0,0 +1,16 @@
+public class HeapMainApp {
+    public static void main(String[] args) {
+
+        MyHeap heap = new MyHeap(5);
+
+        heap.insert(61);
+        heap.insert(120);
+        heap.insert(52);
+        heap.insert(45);
+        heap.insert(83);
+        heap.printHeap();
+        System.out.println();
+        System.out.println(heap.remove()+ " is removed and size is : "+ heap.size);
+        heap.printHeap();
+    }
+}
